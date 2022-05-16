17e6fc245fe5ffe83e8626c499348d61486c70af
DispVAG <101806264+DISPVAG@users.noreply.github.com>
2022-05-15 23:22:32 -0300

    Add files via upload

---
+++ Projeto/api/sprint 2/setting.py
@@ -0,0 +1,3 @@
+STATIC_URL = ('/static/')       #definir o diretório da pasta static, pasta que vai pode ser usado em todos os aquivos
+
+template_folder= ('/templates/') #definir o diretório da pasta static, pasta que vai pode ser usado em todos os aquivos
